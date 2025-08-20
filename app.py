e95da4e0849bcb3ad450c81400f0c328740b4bee
Cod3Talk3r <mohammadr3za.1@gmail.com>
2025-08-20 21:44:41 +0330

    i changed to all_clicks.

--- app.py
+++ app.py
@@ -3,19 +3,19 @@ from random import randint
 
 app = Flask(__name__)
 
-i = 0
+all_clicks = 0
 
 
 @app.route("/")
 def home():
-    global i
+    global all_clicks
     r = randint(0, 255)
     g = randint(0, 255)
     b = randint(0, 255)
-    i += 1
+    all_clicks += 1
     color = tuple((r, g, b))
 
-    return render_template("index.html", i=i, color=color)
+    return render_template("index.html", i=all_clicks, color=color)
 
 
 if __name__ == "__main__":
